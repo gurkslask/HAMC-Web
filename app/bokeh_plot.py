ff1e57110bb4d9c3a6b1dee3be3cdf61210025ae
Alexander Svensson <gurkslask@gmail.com>
2014-11-21 13:06:18 +0100

    added colors

--- app/bokeh_plot.py
+++ app/bokeh_plot.py
@@ -7,7 +7,18 @@ import time
 
 
 def ColorPicker():
-    colors = ['#B2DF8A',
+    colors = [
+              '#1f77b4',
+              '#ff7f0e',
+              '#2ca02c',
+              '#d62728',
+              '#9467bd',
+              '#8c564b',
+              '#e377c2',
+              '#7f7f7f',
+              '#bcbd22',
+              '#17becf',
+              '#B2DF8A',
               '#A6CEE3',
               '#33A02C',
               '#FB9A99'
