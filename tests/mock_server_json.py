6adeaea4c4fa9512ee3606205af1854ec6683803
Alexander Svensson <gurkslask@gmail.com>
2016-03-09 14:28:52 +0100

    Json implementation of tests

---
+++ tests/mock_server_json.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+
+import asyncio
+import json
+
+__author__ = 'alexander'
+
+'''mockserver'''
+
+mockvalues = {'test1': 42,
+              'test2': 2,
+              'test3': 64}
+
+class EchoServerClientProtocol(asyncio.Protocol):
+    def __init__(self, hdata):
+        # data_func method
+        self.HAMC_data = hdata
+
+    def connection_made(self, transport):
+        peername = transport.get_extra_info('peername')
+        print('Connection from {}'.format(peername))
+        self.transport = transport
+
+    def data_received(self, data):
+        message = data.decode('utf-8')
+        message = json.loads(message)
+        print(message)
+        for read_or_write in message:
+            if read_or_write is 'w':
+                for value_to_write in message[read_or_write]:
+                    print(value_to_write)
+                    print("Changing key {} from value {} to {}".format(
+                        value_to_write[0], self.HAMC_data[value_to_write[0]], value_to_write[1])) 
+                    # value_to_write = value_to_write.split(',')
+                    self.HAMC_data[value_to_write[0]] = value_to_write[1]
+                self.transport.write(json.dumps({'Done': 1}))
+            elif read_or_write is 'r':
+                data_to_send = {}
+                for value_to_read in message[read_or_write]:
+                    try:
+                        data_to_send[value_to_read] = self.HAMC_data[value_to_read]
+                    except KeyError:
+                        data_to_send[value_to_read] = 'Keyerror'
+                self.transport.write(json.dumps(data_to_send))
+
+        print('Close the client socket')
+        self.transport.close()
+
+    def data_send(self, data):
+        self.transport.write(data)
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    coro = loop.create_server(
+        lambda: EchoServerClientProtocol(mockvalues),
+        '0.0.0.0',
+        5004
+    )
+    server = loop.run_until_complete(coro)
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+
+    server.close()
+    loop.run_until_complete(server.wait_closed())
+    loop.close()
