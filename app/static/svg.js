3d428e175b805c74e5b54712984f517ade818c62
Alexander Svensson <gurkslask@gmail.com>
2016-09-25 21:59:36 +0200

    End of day save

--- app/static/svg.js
+++ app/static/svg.js
@@ -9,21 +9,19 @@ function pump(x, y, ind, larm){
     this.symb = Snap.load( "/static/pump.svg",  function(f) {
         t1 = f.select("#tri");
         c1 = f.select("#cirk");
-        this.t1 = t1;
-        this.c1 = c1;
         s.append(f);
     });
 
 
     this.pumpOn = function() {
         alert('pumpon');
-        this.t1.animate({transform:"r90, s2, t13, -3", fill:"#bada55"}, 1000, mina.easeinout);
-        this.c1.animate({fill:"000000"}, 2000);
+        t1.animate({transform:"r90, s2, t13, -3", fill:"#bada55"}, 1000, mina.easeinout);
+        c1.animate({fill:"#FFFFFF"}, 2000);
     };
     this.pumpOff = function() {
         alert('pumpoff');
         t1.animate({transform:"r0, s2, t9, 9", fill:"#FFFFFF"}, 1000, mina.easeinout);
-        c1.animate({fill:"FFFFFF"}, 2000);
+        c1.animate({fill:"#bada55"}, 2000);
     };
 };
 
