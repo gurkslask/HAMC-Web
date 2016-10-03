7998f9037f8371516b4adb84a4f260647ac8dc85
Alexander Svensson <gurkslask@gmail.com>
2016-10-03 13:14:13 +0200

    Fixed link to object in js

--- app/static/svg.js
+++ app/static/svg.js
@@ -1,13 +1,12 @@
 var s = Snap(1200, 1200);
-
-symb = Snap.load( "/static/VS1.svg",  function(f) {
+var test = "hej2";
+var symb = Snap.load( "/static/VS1.svg",  function(f) {
     t1 = f.select("#tri");
     c1 = f.select("#cirk");
-    console.log(f.select("#CP2"));
+    test = f.select("#CP2")
     s.append(f);
 });
 
-    test.animate({fill:"#FFFFFF"}, 200);
 
 function pump(x, y, ind, larm, name) {
     this.x = x;
@@ -19,10 +18,13 @@ function pump(x, y, ind, larm, name) {
     this.pumpOn = function() {
         t1.animate({transform:"r90, s2, t13, -3", fill:"#bada55"}, 1000, mina.easeinout);
         c1.animate({fill:"#FFFFFF"}, 200);
+        test.animate({fill:"#FFFFFF"}, 200);
     };
     this.pumpOff = function() {
         t1.animate({transform:"r0, s2, t9, 9", fill:"#FFFFFF"}, 1000, mina.easeinout);
         c1.animate({fill:"#bada55"}, 200);
+        test.selectAll('circle').animate({fill:"red"}, 200);
+        console.log("hejp");
     };
     this.checkState = function() {
         if (this.ind == 1) {
