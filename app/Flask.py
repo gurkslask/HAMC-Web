6c29cdfde60eb46d177d2b843c1ff520e3c170c3
Alexander Svensson <gurkslask@gmail.com>
2016-10-06 21:51:39 +0200

    end of day save

--- app/Flask.py
+++ app/Flask.py
@@ -7,9 +7,10 @@ from wtforms.validators import Required
 from bokeh_plot import LoadFromSQL, bk_plot_timeline
 from bokeh.embed import components
 from bokeh.resources import INLINE
-from flask_script import Manager
+from flask_script import Manager, Shell
 from flask_sqlalchemy import SQLAlchemy
 import os
+import models
 
 configs = {
     'pi_config': {
@@ -43,38 +44,19 @@ app.config.update(configs[os.environ['FLASK_CONFIG']] or configs['pi_config'])
 PICKLE_LOCATION = '''/home/pi/Projects/HAMC/shared_dict'''
 bootstrap = Bootstrap(app)
 Bower(app)
+db = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
 if app.config['CONNECT_TO_SOCKET_METHOD'] == 'external':
     from connect_to_socket import call_server
 elif app.config['CONNECT_TO_SOCKET_METHOD'] == 'internal':
     from connect_to_socket_internal import call_server
 
-class h_Object(db.Model):
-    __tablename__ = 'objects'
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String, unique = True)
-    value = db.Column(db.Float)
-    # limit_id = db.Column(db.Integer, db.ForeignKey('limits.id')) 
-    limits =  db.relationship('sensor_limit', backref=objects)
-
-    def __repr__(self):
-        return 'Name: {}, Value {}'.format(self.name, self.value)
-
-class sensor_limit(db.Model):
-    __tablename__ = 'limits'
-    id = db.Column(db.Integer, primary_key = True)
-    limit_name = (db.String)
-    limit_value = (db.Float)
-    # objects = db.relationship('h_Object', backref='limit')
-    object_id = db.Column(db.Integer, db.ForeignKey('h_Object.id'))
-
-    def __repr__(self):
-        return 'Name: {}, Value: {}'.format(self.limit_1_name, self.limit_1_value)
-
+def make_shell_context():
+    return dict(app=app, db=db, h_Object=models.h_Object, sensor_limit=models.sensor_limit)
+manager.add_command('shell', Shell(make_context=make_shell_context))
 
 @app.route('/bokeh_bild')
 @app.route('/bokeh_bild/<range>')
